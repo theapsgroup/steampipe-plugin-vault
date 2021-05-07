a3c35741f4b92a4db84dea39cd801e268f4d06e2
Graza <code@graza.io>
2021-05-07 15:26:12 +0100

    feat: system health table + docs

---
+++ vault/table_health.go
@@ -0,0 +1,80 @@
+package vault
+
+import (
+	"context"
+
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+)
+
+type SysHealth struct {
+	Initialized                bool
+	Sealed                     bool
+	Standby                    bool
+	PerformanceStandby         bool
+	ReplicationPerformanceMode string
+	ReplicationDrMode          string
+	ServerTimeUtc              int64
+	Version                    string
+	ClusterName                string
+	ClusterID                  string
+}
+
+func tableSysHealth() *plugin.Table {
+	return &plugin.Table{
+		Name:        "vault_sys_health",
+		Description: "Health of Vault",
+		Get: &plugin.GetConfig{
+			KeyColumns: plugin.SingleColumn("cluster_id"),
+			Hydrate:    foo,
+		},
+		List: &plugin.ListConfig{
+			Hydrate: getSysHealth,
+		},
+		Columns: []*plugin.Column{
+			{Name: "initialized", Type: proto.ColumnType_BOOL, Description: "Is Initialized"},
+			{Name: "sealed", Type: proto.ColumnType_BOOL, Description: "Is sealed"},
+			{Name: "standby", Type: proto.ColumnType_BOOL, Description: "Is in standby"},
+			{Name: "performance_standby", Type: proto.ColumnType_BOOL, Description: "Is Performance Standby"},
+			{Name: "replication_performance_mode", Type: proto.ColumnType_STRING, Description: "Replication Performance Mode"},
+			{Name: "replication_dr_mode", Type: proto.ColumnType_STRING, Description: "Replication Disaster Recovery Mode"},
+			{Name: "server_time_utc", Type: proto.ColumnType_INT, Description: "Server Time in UTC (epoch/timestamp)"},
+			{Name: "version", Type: proto.ColumnType_STRING, Description: "Hashicorp Vault Version"},
+			{Name: "cluster_name", Type: proto.ColumnType_STRING, Description: "Name of Vault Cluster"},
+			{Name: "cluster_id", Type: proto.ColumnType_STRING, Description: "Identity of Vault Cluster"},
+		},
+	}
+}
+
+func getSysHealth(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	conn, err := connect(ctx, d)
+
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := conn.Sys().Health()
+
+	if err != nil {
+		return nil, err
+	}
+
+	d.StreamListItem(ctx, &SysHealth{
+		Initialized:                data.Initialized,
+		Sealed:                     data.Sealed,
+		Standby:                    data.Standby,
+		PerformanceStandby:         data.PerformanceStandby,
+		ReplicationPerformanceMode: data.ReplicationPerformanceMode,
+		ReplicationDrMode:          data.ReplicationDRMode,
+		ServerTimeUtc:              data.ServerTimeUTC,
+		Version:                    data.Version,
+		ClusterName:                data.ClusterName,
+		ClusterID:                  data.ClusterID,
+	})
+
+	return nil, nil
+}
+
+func foo(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	return nil, nil
+}
