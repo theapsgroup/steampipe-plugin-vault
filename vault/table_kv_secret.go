3c1374ecac1e94c0bd5eaa12b53b76d0045567e6
Subhi Dweik <subhi@dweik.xyz>
2021-05-12 13:46:12 +0200

    feat: add metadata to secrets output

--- vault/table_kv_secret.go
+++ vault/table_kv_secret.go
@@ -2,8 +2,10 @@ package vault
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/hashicorp/vault/api"
 	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
@@ -14,8 +16,12 @@ import (
 // Key is the path within the mountpoint.
 // Path is the name of the engine
 type KvSecret struct {
-	Key  string
-	Path string
+	Key          string
+	Path         string
+	CreatedTime  time.Time
+	DeletionTime time.Time
+	Destroyed    bool
+	Version      int64
 }
 
 // Defines the table structure and functions to get vault kv secret data
@@ -27,12 +33,16 @@ func tableKvSecret() *plugin.Table {
 			Hydrate: listSecrets,
 		},
 		Get: &plugin.GetConfig{
-			KeyColumns: plugin.AllColumns([]string{"key", "mountpoint"}),
+			KeyColumns: plugin.AllColumns([]string{"key", "path"}),
 			Hydrate:    getSecret,
 		},
 		Columns: []*plugin.Column{
 			{Name: "key", Type: proto.ColumnType_STRING, Description: "The key/path of the kv secret"},
 			{Name: "path", Type: proto.ColumnType_STRING, Description: "The path (mount point) of the secrets engine"},
+			{Name: "created_time", Type: proto.ColumnType_TIMESTAMP, Description: "The path (mount point) of the secrets engine"},
+			{Name: "deletion_time", Type: proto.ColumnType_TIMESTAMP, Description: "The path (mount point) of the secrets engine"},
+			{Name: "destroyed", Type: proto.ColumnType_BOOL, Description: "The path (mount point) of the secrets engine"},
+			{Name: "version", Type: proto.ColumnType_INT, Description: "The path (mount point) of the secrets engine"},
 		},
 	}
 }
@@ -48,6 +58,37 @@ func getSecretAsStrings(ctx context.Context, s *api.Secret) []string {
 
 	}
 	return secrets
+}
+
+// Returns the metadata of a secret, or nil if no secret was found
+func getSecretMetadata(ctx context.Context, client *api.Client, engine string, keyPath string) (*KvSecret, error) {
+	data, err := client.Logical().Read(replaceDoubleSlash(fmt.Sprintf("/%s/metadata/%s", engine, keyPath)))
+
+	if err != nil {
+		return nil, err
+	}
+	if data == nil {
+		return nil, nil
+	}
+
+	secret := &KvSecret{Path: engine, Key: keyPath}
+
+	createdTime, err := time.Parse(time.RFC3339Nano, fmt.Sprintf("%s", data.Data["created_time"]))
+	if err == nil {
+		secret.CreatedTime = createdTime
+	}
+
+	deletionTime, err := time.Parse(time.RFC3339Nano, fmt.Sprintf("%s", data.Data["deletion_time"]))
+	if err == nil {
+		secret.DeletionTime = deletionTime
+	}
+
+	secret.Version, _ = data.Data["current_version"].(json.Number).Int64()
+	// The returned structure contains a map of versions and their properties. E.g. {..., "versions": { "1": { "destroyed": true } } }
+	// This line walks tha tree and fetches the "destroyed" property of the current version
+	secret.Destroyed = data.Data["versions"].(map[string]interface{})[fmt.Sprintf("%d", secret.Version)].(map[string]interface{})["destroyed"].(bool)
+
+	return secret, nil
 }
 
 // Lists all secrets in a secret engine, this has to be done recursively because you only get everything in a "folder"
@@ -68,13 +109,6 @@ func listKvSecrets(ctx context.Context, client *api.Client, engine string, keyPa
 	return secrets, err
 }
 
-// Checks whether a secret exists, used for the get single secret call
-// Returns a bool to make sure we don't leak the values of a secret
-func secretExists(ctx context.Context, client *api.Client, engine string, keyPath string) (bool, error) {
-	data, err := client.Logical().Read(replaceDoubleSlash(fmt.Sprintf("/%s/metadata/%s", engine, keyPath)))
-	return data != nil, err
-}
-
 // The function called by steampipe to populate the table. Will recursively fetch all secrets
 func listSecrets(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
 	conn, err := connect(ctx, d)
@@ -87,7 +121,8 @@ func listSecrets(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData
 		if mounts[path].Type == "kv" {
 			secrets, _ := listKvSecrets(ctx, conn, path, "")
 			for _, k := range secrets {
-				d.StreamListItem(ctx, &KvSecret{Path: path, Key: k})
+				secret, _ := getSecretMetadata(ctx, conn, path, k)
+				d.StreamListItem(ctx, secret)
 			}
 		}
 	}
@@ -107,14 +142,13 @@ func getSecret(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) 
 	keyPath := quals["key"].GetStringValue()
 	mountpoint := quals["path"].GetStringValue()
 
-	data, err := secretExists(ctx, conn, mountpoint, keyPath)
+	data, err := getSecretMetadata(ctx, conn, mountpoint, keyPath)
 
 	if err != nil {
 		return nil, err
 	}
-	if data {
-		return &KvSecret{Path: mountpoint, Key: keyPath}, nil
+	if data == nil {
+		return nil, nil
 	}
-
-	return nil, nil
+	return data, nil
 }
